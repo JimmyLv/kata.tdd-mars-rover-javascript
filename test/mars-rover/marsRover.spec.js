c11ae2e0d986a54a681d7fd60019248100706f67
Jimmy Lv <jinglv@thoughtworks.com>
2016-01-19 21:32:09 +0800

    [#1] Remove semicolon.

--- test/mars-rover/marsRover.spec.js
+++ test/mars-rover/marsRover.spec.js
@@ -12,14 +12,14 @@ describe("MarsRover TDD Dojo", ()=>{
   describe("#received single command", ()=>{
     it("should return 00W when given MarsRover with 00N received L command", ()=>{
       // given
-      var marsRover = new MarsRover();
+      var marsRover = new MarsRover()
 
       // when
-      marsRover.execute("L");
+      marsRover.execute("L")
 
       // then
-      expect(marsRover.status()).to.equal("00W");
-    });
+      expect(marsRover.status()).to.equal("00W")
+    })
   })
 
   describe("#received multiple commands", ()=>{
