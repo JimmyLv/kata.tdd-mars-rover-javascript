dabedd2f24ecbbac93bed3f7d0cbf1fa9ebde892
Jimmy Lv <jimmy.jinglv@gmail.com>
2017-03-21 21:48:47 +0800

    [#1] - Just init project and write simple tests

--- test/mars-rover/marsRover.spec.js
+++ test/mars-rover/marsRover.spec.js
@@ -1,30 +1,30 @@
-import {expect} from "chai"
-import {MarsRover} from "../../src/mars-rover/marsRover.js"
+import { expect } from "chai"
+import { MarsRover } from "../../src/mars-rover/marsRover.js"
 
-describe("first test", ()=> {
-  it("should pass when 1+1=2", ()=> {
+describe("first test", () => {
+  it("should pass when 1+1=2", () => {
     expect(1 + 1).to.equal(2)
   })
 })
 
 
-describe("MarsRover TDD Dojo", ()=> {
-  describe("#received single command", ()=> {
+describe("MarsRover TDD Dojo", () => {
+  describe("#received single command", () => {
 
     const TEST_DATA = [
       [new MarsRover(), "L", "00W"],
       [new MarsRover(), "R", "00E"]
     ]
 
-    TEST_DATA.forEach(([marsRover, command, status])=> {
-      it("should return " + status + " when given MarsRover with" + marsRover + " received" + command + " command", ()=> {
-        marsRover.execute(command);
-        expect(marsRover.status()).to.equal(status);
+    TEST_DATA.forEach(([marsRover, command, status]) => {
+      it("should return " + status + " when given MarsRover with" + marsRover + " received" + command + " command", () => {
+        marsRover.execute(command)
+        expect(marsRover.status()).to.equal(status)
       })
     })
   })
 
-  describe("#received multiple commands", ()=> {
+  describe("#received multiple commands", () => {
 
   })
 
